2600b44156aea7676630afd2ffbb487aff604cbd
koshinus <vergile@yandex.ru>
2016-10-31 05:26:40 +0500

    Add tests for MessageStore

--- src/main/java/ru/urfu/MessageStorage.java
+++ src/main/java/ru/urfu/MessageStorage.java
@@ -16,23 +16,47 @@ public class MessageStorage
 {
     private static final Map<String, Message> _messages = new HashMap<>();
 
-    @RequestMapping(method = RequestMethod.POST)
-    @ResponseBody
-    void addMessages(@RequestParam("mes") String mes)
+    private String generateNewKey()
     {
         char _id[] = new char[11];
         Message check = _messages.get(new String(_id));
+        String alphabet =
+                "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        Random rand = new Random();
         while (check != null)
         {
-            String alphabet =
-                    "0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-            Random rand = new Random();
             for (int i = 0; i < _id.length; i++)
                 _id[i] = alphabet.charAt(rand.nextInt(alphabet.length()));
+            check = _messages.get(new String(_id));
         }
-        _messages.put(new String(_id), new Message(mes));
-
+        return new String(_id);
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    @ResponseBody
+    void addMessage(@RequestParam("mes") String mes)
+    {
+        _messages.put(generateNewKey(), new Message(mes));
+    }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    //@ResponseBody
+    String addMessage(@RequestParam Map.Entry<String, Message> mes) //throws Exception
+    {
+        String key = mes.getKey();
+        Message check = _messages.get(key);
+        if(key.length() != 11 || check != null)
+        {
+            String newKey = generateNewKey();
+            _messages.put(newKey, mes.getValue());
+            return newKey;
+        }
+        //throw new Exception("Wrong key length!");
+        //if(check != null) throw new Exception("Key is already use!");
+        _messages.put(key, mes.getValue());
+        return key;
+    }
+
 
     static
     {
@@ -40,11 +64,16 @@ public class MessageStorage
         _messages.put("00000000002", new Message("Здесь будет новое сообщение"));
     }
 
+    public void printMessageStore()
+    {
+        System.out.println(_messages);
+    }
+
     @RequestMapping(value = "/messages/{id}", method = RequestMethod.GET)
     @ResponseBody
-    String renderMessageById(@RequestParam("id") String _id)
+    String renderMessageById(@RequestParam("id") String id)
     {
-        String message = _messages.get(_id).getMessage();
+        String message = _messages.get(id).getMessage();
         return
                 "<html>" +
                 "   <link rel=\"stylesheet\" type=\"text/css\" href=\"/twitter.css\"/>" +
@@ -52,7 +81,7 @@ public class MessageStorage
                 "       <h1>twitter</h1>" +
                 "       This is your twitter application" +
                 "       <ul class=\"messages\">" +
-                        message +
+                        "<li>" + message + "</li>" +
                 "       </ul>" +
                 "   </body>" +
                 "</html>";
@@ -65,9 +94,8 @@ public class MessageStorage
         String messages = _messages
             .values()
             .stream()
-            .map(msg -> "<li>" + msg+ "</li>")
+            .map(msg -> "<li>" + msg.getMessage() + "</li>")
             .collect(Collectors.joining());
-
         return
             "<html>" +
             "   <link rel=\"stylesheet\" type=\"text/css\" href=\"/twitter.css\"/>" +
@@ -83,9 +111,9 @@ public class MessageStorage
 
     @RequestMapping(value = "/messages/{id}", method = RequestMethod.DELETE)
     @ResponseBody
-    void deleteMessage(@RequestParam("id") String _id)
+    void deleteMessage(@RequestParam("id") String id)
     {
-        _messages.remove(_id);
+        _messages.remove(id);
     }
 
 }
